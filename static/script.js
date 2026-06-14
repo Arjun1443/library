e5229db5274f34385924213f6bb4d8d39ef13ec1
Arjun1443 <nreddyjanga@gmail.com>
2026-06-14 18:13:26 +0530

    Add new files

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'librarysample-ghgjfmhnhxasgtdb.canadacentral-01.azurewebsites.net';
 
 // Load books when page loads
 document.addEventListener('DOMContentLoaded', () => {
